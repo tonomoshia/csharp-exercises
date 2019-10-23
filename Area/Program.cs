d5294c2eceb78d917f683182902e6cc1683e1bc4
Elizabeth Reiher <tonomoshia@gmail.com>
2019-10-22 20:24:00 -0400

    program to compute area of a circle given the radius.

--- Area/Program.cs
+++ Area/Program.cs
@@ -6,7 +6,11 @@ namespace Area
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a radius: ");
+            double radius = double.Parse(Console.ReadLine());
+            double area = 3.14 * radius * radius;
+            Console.WriteLine($"The area of a circle with radius {radius} is: {area}.");
+            Console.ReadLine();
         }
-    }
+}
 }
