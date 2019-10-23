3da3eea6317f493ce0ed3a4de44d48a264bcfbcb
Tonomoshia <tonomoshia@gmail.com>
2019-10-22 23:32:57 -0400

    added project to calculate the area of a rectangle with input from user as to width and height

---
+++ RectangleArea/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the width of the rectangle: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the height of the rectangle: ");
+            double height = double.Parse(Console.ReadLine());
+            double area = width * height;
+            Console.WriteLine($"The area of a rectangle with width {width} and height {height} is: {area}.");
+            Console.ReadLine();
+		}
+    }
+}
