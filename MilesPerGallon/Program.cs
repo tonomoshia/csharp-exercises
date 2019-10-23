fa026c3392da84d7a2b1887892dbea03fe8a721d
Tonomoshia <tonomoshia@gmail.com>
2019-10-22 23:41:57 -0400

    added project to calculate miles per gallon given user input of miles driven and gallons of gas used.

---
+++ MilesPerGallon/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MilesPerGallon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many miles have you driven today?");
+            double miles = double.Parse(Console.ReadLine());
+            Console.WriteLine("How many gallons of gas have you used today?");
+            double gallons = double.Parse(Console.ReadLine());
+            double mpg = miles/gallons;
+            Console.WriteLine($"You are getting {mpg} miles per gallon!");
+            Console.ReadLine();
+        }
+    }
+}
