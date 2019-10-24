efc4056ae41fda5e4a1065445123efa4b00b1a42
Tonomoshia <tonomoshia@gmail.com>
2019-10-24 14:09:26 -0400

    Ask user for input and return message using the user input.

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -6,16 +6,25 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            int x;
-            int y;
+            //int x;
+            //int y;
 
-            x = 7;
-            y = x + 3;
+            //x = 7;
+            //y = x + 3;
 
-            Console.WriteLine(y);
+            //Console.WriteLine(y);
+            //Console.ReadLine();
+
+            //Console.WriteLine("Hello World!");
+
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            string myFirstName = Console.ReadLine();
+            Console.Write("Type your last name: ");
+            string myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + myFirstName + " " + myLastName + ".");
             Console.ReadLine();
-
-            Console.WriteLine("Hello World!");
         }
     }
 }
