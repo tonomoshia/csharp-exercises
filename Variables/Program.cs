4c81e20c3345d1ae381ac276e5466f345feca1f2
Tonomoshia <tonomoshia@gmail.com>
2019-10-23 21:05:37 -0400

    Auto stash before revert of "playing with variables - integers and strings"

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -6,7 +6,17 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+
             Console.WriteLine("Hello World!");
+
         }
     }
 }
