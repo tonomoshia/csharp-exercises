87ffffee4af1d494b555fe4918be8e3c2c1889a9
Tonomoshia <tonomoshia@gmail.com>
2019-10-25 14:02:10 -0400

    break statement stops the for loop when it hits 7, does not print list of integers.

--- ForIteration/Program.cs
+++ ForIteration/Program.cs
@@ -12,7 +12,12 @@ namespace ForIteration
         {
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine(i);
+                //Console.WriteLine(i);
+                if (i==7)
+                {
+                    Console.WriteLine("Found seven!");
+                    break;
+                }
             }
             Console.ReadLine();
         }
