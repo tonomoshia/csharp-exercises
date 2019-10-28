cce1bf9e442cb2e49d8d7ef218f5ca46f6f83721
Elizabeth Reiher <tonomoshia@gmail.com>
2019-10-28 13:30:18 -0400

    create WorkingWithStrings

---
+++ WorkingWithStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "";
+
+            Console.WriteLine(myString);
+            Console.ReadLine();
+        }
+    }
+}
