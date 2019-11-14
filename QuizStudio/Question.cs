01b992d85f51fe1d2e4b1bfcc9d5dbaacf5529b6
Elizabeth Reiher <tonomoshia@gmail.com>
2019-11-13 20:30:57 -0500

    working Restaurant and Quiz studios. not functional yet.

---
+++ QuizStudio/Question.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quiz
+{
+    public class Question
+    {
+        public string question;
+
+        public static addQuestion(q);
+    }
+    // Multiple Choice: a question with a fixed set of possible answers, of which only one may be chosen and only one answer is correct
+    public class MultipleChoiceQuestion : Question
+    {
+
+    }
+    // Checkbox: a question with a fixed set of possible answers, of which any number may be chosen; there is one correct combination of choices
+    public class CheckboxQuestion : Question
+    {
+
+    }
+    // True?false: a question that has a true/false answer
+    public class TrueFalseQuestion : Question
+    {
+        readonly bool answer;
+    }
+}
